a30ca40f85ecae377e6e9a08c401982d69c14a98
BrijeshMakwana18 <bamakwana2000@gmail.com>
2021-10-09 15:58:55 +0530

    ui design completed

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
 import styles from './styles';
 import {fonts, images, colors} from '../../theme';
+
 export default function Landing() {
   return (
     <View style={styles.container}>
-      <Text>Welcome</Text>
+      <Image source={images.logo} style={styles.logo} />
+      <View style={styles.landingContentContainer}>
+        <Text style={styles.landingTitle}>
+          {"Let's connect\nwith each other"}
+        </Text>
+        <Text style={styles.landingTitleTagline}>
+          {
+            'A message is discrete communication\nintented by the source consumption.'
+          }
+        </Text>
+        <TouchableOpacity style={styles.buttonContainer}>
+          <Text style={styles.buttonTitle}>{"Let's Start"}</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- src/pages/Landing/styles.js
+++ src/pages/Landing/styles.js
@@ -1,9 +1,55 @@
 import {StyleSheet} from 'react-native';
+import {colors, fonts} from '../../theme';
 
 export default styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: '10%',
+  },
+  logo: {
+    height: '50%',
+    width: '100%',
+    resizeMode: 'contain',
+    // backgroundColor: 'green',
+  },
+  landingContentContainer: {
+    flex: 1,
+    height: '100%',
+    width: '100%',
+    backgroundColor: colors.secondaryBackgroundColor,
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  landingTitle: {
+    fontFamily: fonts.quicksandBold,
+    color: colors.primaryTextColor,
+    fontSize: 30,
+    textAlign: 'center',
+  },
+  landingTitleTagline: {
+    fontFamily: fonts.quicksandBold,
+    color: colors.primaryTextColor,
+    fontSize: 18,
+    opacity: 0.5,
+    textAlign: 'center',
+    marginTop: '5%',
+  },
+  buttonContainer: {
+    height: '15%',
+    width: '50%',
+    marginTop: '10%',
+    backgroundColor: colors.buttonBackgroundColor,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonTitle: {
+    fontFamily: fonts.quicksandBold,
+    color: colors.primaryTextColor,
+    fontSize: 20,
   },
 });
